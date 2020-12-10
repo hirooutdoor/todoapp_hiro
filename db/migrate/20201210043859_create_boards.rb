057ea3d60766f37ce0fb0418aaefdd716f542975
hiro <>
2020-12-10 14:43:34 +0900

    ・MVC実行（モデル・ビュー・コントローラの紐付け確認） ・schemaのcontentをtextに変更 rollback→migrate

--- db/migrate/20201210043859_create_boards.rb
+++ db/migrate/20201210043859_create_boards.rb
@@ -2,7 +2,7 @@ class CreateBoards < ActiveRecord::Migration[6.0]
   def change
     create_table :boards do |t|
       t.string :title, null: false
-      t.string :content, null: false
+      t.text :content, null: false
       t.timestamps
     end
   end
