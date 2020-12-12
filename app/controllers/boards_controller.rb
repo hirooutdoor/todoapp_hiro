c844f19644d34484f666d2348e2d1f82e1a2aa0b
hiro <>
2020-12-12 11:38:02 +0900

    createアクション定義

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -4,8 +4,25 @@ class BoardsController < ApplicationController
   end
 
   def new
-    @boards = Board.create
+    @board = Board.new
   end
 
+  def show
+    @board = Board.find(params[:id])
+  end
+
+  def create
+    @board = Board.new(board_params)
+    if @board.save
+      redirect_to board_path(@board)
+    else
+      render :new
+    end
+  end
+
+  private
+  def board_params
+    params.require(:board).permit(:title, :content)
+  end
 
 end
