6e01e19ffba0ffbf057c73ed1a4f79d70b3f22d0
hiro <>
2020-12-13 15:22:34 +0900

    profiles_controllerにてeditとupdateアクションの定義

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -4,12 +4,31 @@ class ProfilesController < ApplicationController
 
     def show
         @profile = current_user.profile
-        # has_one: profileだからprofileでok
     end
 
     def edit
-        @profile = current_user.build_profile
-        #has_oneの場合の書き方
+        @profile = current_user.prepare_profile
     end
 
+    def update
+        @profile = current_user.prepare_profile
+        @profile.assign_attributes(profile_params)
+        if @profile.save
+            redirect_to profile_path, notice: 'プロフィール更新できました'
+        else
+            flash.now[:error] = '更新できませんでした'
+            render :edit
+        end
+    end
+
+    private
+    def profile_params
+        params.require(:profile).permit(
+            :nickname,
+            :introduction,
+            :gender,
+            :birthday,
+            :subscribed
+        )        
+    end
 end
