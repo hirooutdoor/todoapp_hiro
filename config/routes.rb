d69fead07886ac44cc0286c34ee61c964d671cbc
hiro <>
2020-12-14 06:54:07 +0900

    routesにnewを追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'boards#index'
   resources :boards do
-    resources :tasks, only: [:show, :edit, :update, :create, :delete]
+    resources :tasks, only: [:show, :new, :edit, :update, :create, :delete]
   end
   resource :profile, only: [:show, :edit, :update]
 end
