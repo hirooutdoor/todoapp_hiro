d5373b86ece93487f453747a9d2873aa4e7dd733
hiro <>
2020-12-13 16:14:58 +0900

    プロフィールページ ニックセームを表示 年齢を表示 自己紹介文を表示

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,16 +5,25 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_one :profile, dependent: :destroy
-
   has_many :boards, dependent: :destroy
+
+  delegate :birthday, :age, :gender, to: :profile, allow_nil: true
 
   def has_written?(board)
     boards.exists?(id: board.id)    
   end
 
   def display_name
-    self.email.split('@').first
+    profile&.nickname || self.email.split('@').first
   end
+
+  # def birthday
+  #   profile&.birthday
+  # end
+
+  # def gender
+  #   profile&.gender
+  # end
 
   def prepare_profile
     profile || build_profile
