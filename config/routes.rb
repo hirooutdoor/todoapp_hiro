b1e65f4f21703a2f23aafd4a78eee17eebe355a6
hiro <>
2020-12-14 06:26:29 +0900

    routingの修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'boards#index'
-  resources :boards
-  resources :tasks
-
+  resources :boards do
+    resources :tasks, only: [:show, :edit, :update, :create, :delete]
+  end
   resource :profile, only: [:show, :edit, :update]
 end
   
