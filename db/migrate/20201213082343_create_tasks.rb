fcc92915ebd7a07a2542feb22af1b98db4615f85
hiro <>
2020-12-14 06:26:00 +0900

    taskとboardの紐付け

--- db/migrate/20201213082343_create_tasks.rb
+++ db/migrate/20201213082343_create_tasks.rb
@@ -2,9 +2,9 @@ class CreateTasks < ActiveRecord::Migration[6.0]
   def change
     create_table :tasks do |t|
       t.references :user, null: false
+      t.references :board, null: false
       t.string :title, null: false
       t.text :content, null: false
-      
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -38,10 +38,12 @@ ActiveRecord::Schema.define(version: 2020_12_13_082343) do
 
   create_table "tasks", force: :cascade do |t|
     t.bigint "user_id", null: false
+    t.bigint "board_id", null: false
     t.string "title", null: false
     t.text "content", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["board_id"], name: "index_tasks_on_board_id"
     t.index ["user_id"], name: "index_tasks_on_user_id"
   end
 
