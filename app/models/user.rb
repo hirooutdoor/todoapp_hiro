551685f220f847e3325109af0204566398350bfa
hiro <>
2020-12-13 13:54:25 +0900

    migrationファイルの設定 モデルの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_one :profile, dependent: :destroy
+
   has_many :boards, dependent: :destroy
 
   def has_written?(board)
