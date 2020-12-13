a26ecc66405594d3d17f5f100e0727bd9c969e9b
hiro <>
2020-12-14 06:57:08 +0900

    tasks_controller createとeditメソッドにひとまず追記

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -9,14 +9,15 @@ class TasksController < ApplicationController
     end
 
     def edit
+        @task = current_user.tasks.build
     end
 
     def create
-
+        @task = current_user.tasks.new
+        @task.save
     end
 
     def update
-
     end
 
     def destroy
