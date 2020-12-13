70cb6ffe617e3e30e9f759cb14fc8e63a90aa96e
hiro <>
2020-12-13 13:36:32 +0900

    author_nameの設定

--- app/models/board.rb
+++ app/models/board.rb
@@ -11,4 +11,9 @@ class Board < ApplicationRecord
     def display_created_at
         I18n.l(self.created_at, format: :default)
     end
+
+    def author_name
+        user.display_name
+    end
+
 end
