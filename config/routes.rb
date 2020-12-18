42536b1fef6657c8f30a3722a323ef2480293513
hiro <>
2020-12-18 21:22:58 +0900

    routes/resources:tasks のdestroy定義（deleteになってた）

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'boards#index'
   resources :boards do
-    resources :tasks, only: [:show, :new, :edit, :update, :create, :delete]
+    resources :tasks, only: [:show, :new, :edit, :update, :create, :destroy]
   end
   resource :profile, only: [:show, :edit, :update]
 end
