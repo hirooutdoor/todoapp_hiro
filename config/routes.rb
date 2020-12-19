1d16007958bae78802ce605a5eaba28993dee96e
hiro <>
2020-12-20 08:07:50 +0900

    UserとTaskとのリレーション task内にresources(task_idにルーティング)

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'boards#index'
   resources :boards do
-    resources :tasks, only: [:show, :new, :edit, :update, :create, :destroy]
+    resources :tasks, only: [:show, :new, :edit, :update, :create, :destroy] do
+      resources :comments, only: [:new, :create]
+    end
   end
   resource :profile, only: [:show, :edit, :update]
 end
